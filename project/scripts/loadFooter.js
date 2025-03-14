dc83c9e76d5c503036ae49859b3adb9a56563090
Денис <denis.k0walenko@yandex.ru>
2025-03-14 16:53:13 +0300

    added header + footer components, added a welcome page + approval page for splitting 3d model

---
+++ project/scripts/loadFooter.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("../components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-container").innerHTML = data;
+    })
+    .catch((error) => console.error("Ошибка загрузки футера:", error));
+});
